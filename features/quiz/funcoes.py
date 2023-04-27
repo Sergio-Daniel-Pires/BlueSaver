a7e88526f72d4249e0374080cddef60876382e2e
TorMenezes <tormenezessouza@gmail.com>
2023-04-27 20:55:28 -0300

    alterações na feature quiz

--- features/quiz/funcoes.py
+++ features/quiz/funcoes.py
@@ -1,17 +1,40 @@
 import json
 import os
+from flask import Flask, Response
 
 def escolher_perguntas(dificuldade: str, arquivos_estaticos: str) -> dict:
     """
     Com base numa dificuldade, devolve perguntas dela.
     """
-    print(f"Seu quiz tem dificuldade {dificuldade}")
+    #print(f"Seu quiz tem dificuldade {dificuldade}")
 
     with open(os.path.join(arquivos_estaticos, 'perguntas.json'), "r") as perguntas_json:
         perguntas = json.loads(perguntas_json.read())
 
-    for numquestao in perguntas[dificuldade] :
-        print(perguntas[dificuldade][numquestao]['Pergunta'])
+   
 
-        for alternativa in perguntas[dificuldade][numquestao]['Opcoes'] :
-            print(alternativa,") ",perguntas[dificuldade][numquestao]['Opcoes'][alternativa])
+    #linhas = []
+    nivel=perguntas[dificuldade]
+    new_dict = {}
+
+    for questao in nivel:
+        new_dict+=  nivel[questao]['Pergunta']
+        new_dict+= nivel[questao]['Opcoes']
+
+    return new_dict
+    """
+    return dict(nivel)
+    out=""
+    for q in nivel: 
+        out += f"Pergunta: {nivel[q]['Pergunta']}  a"
+        out += "\n"
+    """
+    """   
+    response = make_response({'result': out})
+    print(type(response))
+    response.headers['Content-Type'] = 'text/markdown'
+    #"\n".join(linhas)
+    return response
+    #return dict(perguntas)  
+    """
+
