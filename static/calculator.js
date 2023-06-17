cbafda9c47d3523233884693a130403d750cc237
TorMenezes <tormenezessouza@gmail.com>
2023-06-17 14:26:03 -0300

    atualizações na calculadora

--- static/calculator.js
+++ static/calculator.js
@@ -3,12 +3,16 @@ function calculator(){
     // Obter os valores do formulário
     var qtd_pessoas = parseFloat(document.getElementById('qtd_pessoas').value);
     var gasto_mensal = parseFloat(document.getElementById('gasto_mensal').value);
+    var estado = document.getElementById('estado').value;
 
     if (isNaN(qtd_pessoas)|| isNaN(gasto_mensal)) {
         alert('Por favor, preencha todos os campos obrigatórios.');
     }
     else if( !Number.isInteger(parseFloat(qtd_pessoas)) || !Number.isInteger(parseFloat(gasto_mensal))){
         alert('Por favor, preencha os campos números com valores inteiros.');
+    }
+    else if (estado === ""){
+        alert('Por favor, selecione seu estado.');
     }
     else{
         // Realizar o cálculo
