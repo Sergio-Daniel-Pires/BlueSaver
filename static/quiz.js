ee1645f3fe20a9c3fa193e1af8681de28d9c76f0
Gabriel Mikio <gmikioas@gmail.com>
2023-06-19 20:36:06 -0300

    Implementando visualização de 1 questão por vez no quiz

--- static/quiz.js
+++ static/quiz.js
@@ -21,99 +21,107 @@ window.onload = function () {
                 type: 'POST',
                 data: { Dificuldade: dificuldade },
                 success: function(response) {
-                    // Limpar as perguntas e respostas anteriores
-                    $('#perguntas').empty();
-                    $('#respostas').empty();
-
-                    // Preencher as perguntas e respostas
-                    var letras = ['a', 'b', 'c'];
-                    var perguntasTable = $('<table>').addClass('table');
-                    var respostasTable = $('<table>').addClass('table');
-                    $.each(response, function(key, value) {
-                        var numero = key;
-                        var pergunta = value['Pergunta'];
-                        var opcoes = value['Opcoes']
-
-                        // Div Pergunta
-                        var row = $('<tr>');
-                        var cell = $('<td>').text(pergunta);
-                        var perguntasCell = $('<td>').text("Pergunta " + numero);
-                        row.append(perguntasCell);
-                        row.append(cell);
-                        perguntasTable.append(row);
-                        
-                        var row = $('<tr>');
-                        var respostasCell = $('<td>').text("Resposta " + numero);
-                        row.append(respostasCell);
-                        $.each(opcoes, function(letra, resposta) {
-                            var radioId = 'resposta-' + numero + '-' + letra;
-                            var radio = $('<input>').attr('type', 'radio').attr('name', key.split(' ')[1]).val(letra).attr('id', radioId).addClass('resposta-radio');
-                            var label = $('<label>').text(resposta).attr('for', radioId);
-                            var cell = $('<td>').append(radio).append(label);
-                            row.append(cell);
-                            respostasTable.append(row);
-                        });
-                    });
-
-                    $('#perguntas').append(perguntasTable);
-                    $('#respostas').append(respostasTable);
-
+                    loadQuiz();
                 }
             });
         });
-        $('#verificar').click(function () {
-            dificuldade = false
-            if (document.querySelector(".btn-success")) {
-                dificuldade = 'Fácil';
-            } else if (document.querySelector(".btn-warning")) {
-                dificuldade = 'Médio';
-            } else if (document.querySelector(".btn-danger")) {
-                dificuldade = 'Difícil';
-            }
-            var respostas = {};
-            $('input[class=resposta-radio]:checked').each(function() {
-                respostas[$(this).attr('id').split('-')[1]] = $(this).val();
-            });
-            send = {};
-            send['Dificuldade'] = dificuldade;
-            send['Resposta'] = JSON.stringify(respostas);
+        // $('#verificar').click(function () {
+        //     send = {};
+        //     send['Dificuldade'] = dificuldade;
+        //     send['Resposta'] = JSON.stringify(respostas);
 
-            $.ajax({
-                url: '/quiz/responder',
-                type: 'POST',
-                data: send,
-                success: function (response) {
-                    // Exibir o resultado das respostas
-                    acertos = response['resultado']['acertos'];
-                    total = response['resultado']['total'];
-                    var porcentagemAcertos = (acertos / total) * 100;
-                    var label = "Parabens!";
-                    if (porcentagemAcertos < 50){
-                        label = "Estude mais!";
-                    } 
+        //     $.ajax({
+        //         url: '/quiz/responder',
+        //         type: 'POST',
+        //         data: send,
+        //         success: function (response) {
+        //             // Exibir o resultado das respostas
+        //             acertos = response['resultado']['acertos'];
+        //             total = response['resultado']['total'];
+        //             var porcentagemAcertos = (acertos / total) * 100;
+        //             var label = "Parabens!";
+        //             if (porcentagemAcertos < 50){
+        //                 label = "Estude mais!";
+        //             } 
 
-                    var gauge = new JustGage({
-                        id: "gauge",
-                        value: porcentagemAcertos,
-                        min: 0,
-                        max: 100,
-                        title: "Porcentagem de Acertos",
-                        label: label,
-                        gaugeWidthScale: 0.6,
-                        counter: true,
-                        relativeGaugeSize: true,
-                        levelColors: ["#ff0000", "#ffa500", "#6ab04c"], // Cores para diferentes níveis (opcional)
-                        levelColorsGradient: false, // Gradiente entre as cores (opcional)
-                        startAnimationType: "bounce",
-                        startAnimationTime: 2000,
-                        refreshAnimationType: "bounce",
-                        refreshAnimationTime: 1000
-                    });
-                }
-            });
-        });
+        //             var gauge = new JustGage({
+        //                 id: "gauge",
+        //                 value: porcentagemAcertos,
+        //                 min: 0,
+        //                 max: 100,
+        //                 title: "Porcentagem de Acertos",
+        //                 label: label,
+        //                 gaugeWidthScale: 0.6,
+        //                 counter: true,
+        //                 relativeGaugeSize: true,
+        //                 levelColors: ["#ff0000", "#ffa500", "#6ab04c"], // Cores para diferentes níveis (opcional)
+        //                 levelColorsGradient: false, // Gradiente entre as cores (opcional)
+        //                 startAnimationType: "bounce",
+        //                 startAnimationTime: 2000,
+        //                 refreshAnimationType: "bounce",
+        //                 refreshAnimationTime: 1000
+        //             });
+        //         }
+        //     });
+        // });
     });
 }
+
+const questionElement = document.getElementById("question");
+const optionsElement = document.getElementById("options");
+const submitButton = document.getElementById("submit");
+
+let currentQuestion = 0;
+let score = 0;
+let difficulty = "easy"; // Set the default difficulty level
+
+function loadQuiz() {
+    fetch("quizData.json")
+        .then(response => response.json())
+        .then(data => {
+            const currentQuizData = data[difficulty][currentQuestion];
+
+            questionElement.innerText = currentQuizData.question;
+            optionsElement.innerHTML = "";
+
+            currentQuizData.options.forEach((option, index) => {
+                const li = document.createElement("li");
+                li.innerText = option;
+                li.addEventListener("click", () => selectAnswer(index));
+                optionsElement.appendChild(li);
+            });
+        });
+}
+
+function selectAnswer(selectedIndex) {
+    fetch("quizData.json")
+        .then(response => response.json())
+        .then(data => {
+            const currentQuizData = data[difficulty][currentQuestion];
+
+            if (selectedIndex === currentQuizData.correctAnswer) {
+                score++;
+            }
+
+            currentQuestion++;
+
+            if (currentQuestion < data[difficulty].length) {
+                loadQuiz();
+            } else {
+                showResults();
+            }
+        });
+}
+
+function showResults() {
+    const quizContainer = document.getElementById("quiz-container");
+    quizContainer.innerHTML = `
+    <h2>You scored ${score}/${currentQuestion} correct answers.</h2>
+    <button onclick="location.reload()">Restart Quiz</button>
+  `;
+}
+
+submitButton.addEventListener("click", loadQuiz);
 
 function sound_bubble(){
     if(!muted){
--- views/quiz.py
+++ views/quiz.py
@@ -11,36 +11,38 @@ def generate_quiz():
         return make_response(render_template('quiz.html'), 200)
     
     elif request.method == "POST":
-        """
-        Preencha aqui a função 'receber quiz'
+    #     """
+    #     Preencha aqui a função 'receber quiz'
         
-        Essa função deve retornar as perguntas do quiz no formato JSON.
-        """
-        formulario = dict(request.form)
-        dificuldade = formulario.get('Dificuldade', None)
-        if dificuldade is None:
-            return "Dificuldade não pode ser vazia!", 400
+    #     Essa função deve retornar as perguntas do quiz no formato JSON.
+    #     """
+    #     formulario = dict(request.form)
+    #     dificuldade = formulario.get('Dificuldade', None)
+    #     if dificuldade is None:
+    #         return "Dificuldade não pode ser vazia!", 400
 
-        STATIC = current_app.config['STATIC']
-        perguntas, _ = escolher_perguntas(dificuldade, STATIC)
-        return jsonify(perguntas)
-
-@quiz_bp.route('/responder', methods=['POST'])
-def answer_quiz():
-    """ 
-    Preencha aqui a 'devolver quiz'
+    #     STATIC = current_app.config['STATIC']
+    #     perguntas, _ = escolher_perguntas(dificuldade, STATIC)
+    #     return jsonify(perguntas)
+        return make_response(render_template('quiz.html'), 200)
     
-    Essa função deve receber as respostas das perguntas do quiz e devolver a pontuação.
-    """
-    formulario = dict(request.form)
-    dificuldade = formulario.get('Dificuldade', None)
-    respostas = formulario.get('Resposta', None)
-    if dificuldade is None:
-        return "Dificuldade não pode ser vazia!", 400
-    if respostas is None:
-        return f"Resposta não pode ser vazia!"
+    
+# @quiz_bp.route('/responder', methods=['POST'])
+# def answer_quiz():
+#     """ 
+#     Preencha aqui a 'devolver quiz'
+    
+#     Essa função deve receber as respostas das perguntas do quiz e devolver a pontuação.
+#     """
+#     formulario = dict(request.form)
+#     dificuldade = formulario.get('Dificuldade', None)
+#     respostas = formulario.get('Resposta', None)
+#     if dificuldade is None:
+#         return "Dificuldade não pode ser vazia!", 400
+#     if respostas is None:
+#         return f"Resposta não pode ser vazia!"
 
-    STATIC = current_app.config['STATIC']
-    respostas = json.loads(respostas)
-    resultado = verifica_resposta(dificuldade, respostas, STATIC)
-    return jsonify(resultado)
+#     STATIC = current_app.config['STATIC']
+#     respostas = json.loads(respostas)
+#     resultado = verifica_resposta(dificuldade, respostas, STATIC)
+#     return jsonify(resultado)
