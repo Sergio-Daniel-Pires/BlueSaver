42a51d045b2cd3608d929f6ccfb167ff642625a2
Ygor Jesus <ygor.jesus_ciandt@prestador.globo>
2023-06-29 11:29:57 -0300

    Adiciona teste da rota /calculadora/calcular

--- test_calculadora.py
+++ test_calculadora.py
@@ -18,3 +18,14 @@ def test_calculator_invalid_key_error():
     with app_.test_client() as client:
         with pytest.raises(KeyError):
             response = client.post('/calculadora/calcular', data={'qtd_pessoas': '4', 'gasto_mensal': '100', 'estado': 'São Paulo'})
+
+
+def test_calculator_valid_content_type():
+    """
+    Testa se a resposta do `POST` da rota "/calculadora/calcular"
+    com dados válidos possui o tipo de conteúdo JSON.
+    """
+    with app_.test_client() as client:
+        response = client.post('/calculadora/calcular', data={'qtd_pessoas': '4', 'gasto_mensal': '100', 'estado': 'sudeste'})
+
+    assert response.headers['Content-Type'] == 'application/json'
